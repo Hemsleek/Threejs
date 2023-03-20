17c46b15af7893eeec70ff7cf77b9ea47c7c0c13
Asiyanbi Mubashir <asiyanbimubashir@gmail.com>
2023-03-20 08:56:17 +0100

    feat: local-server position

---
+++ 04-local-server/src/script.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three'
+
+console.log(THREE);
+
+const canvas = document.querySelector("canvas.webgl")
+
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+const material = new THREE.MeshBasicMaterial({ color: 'red' })
+
+const mesh = new THREE.Mesh(geometry, material)
+
+mesh.position.x = 0.7
+mesh.position.y = -0.7
+mesh.position.z = 0.7
+
+//distance/length from center of scene to mesh/object/cube
+console.log(mesh.position.length());
+
+//normalise mesh- reduce it's length to 1
+//mesh.position.length() will equals 1 
+mesh.position.normalize()
+
+mesh.position.set(1, - 1, 1)
+
+
+//AxesHelper
+const axesHelper = new THREE.AxesHelper(3)
+
+scene.add(axesHelper)
+
+scene.add(mesh)
+
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+
+camera.position.z = 3
+
+//distance between the object/mesh/cube and camerq
+console.log(mesh.position.distanceTo(camera.position));
+
+scene.add(camera)
+
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
